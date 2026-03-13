8837050307f87dc2fe8f0b3a26dad51db8b2a722
Ethan Perlus-Rabinovitch <eperl1@ocdsb.ca>
2026-03-12 20:15:15 -0500

    Add missing proxy files

---
+++ public/uv.config.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>TypeError: path must be absolute or specify root to res.sendFile<br> &nbsp; &nbsp;at ServerResponse.sendFile (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\express\lib\response.js:400:11)<br> &nbsp; &nbsp;at file:///c:/Users/Ethan/Desktop/Ultraviolet-App/src/index.js:23:6<br> &nbsp; &nbsp;at Layer.handleRequest (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\router\lib\layer.js:152:17)<br> &nbsp; &nbsp;at trimPrefix (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\router\index.js:342:13)<br> &nbsp; &nbsp;at c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\router\index.js:297:9<br> &nbsp; &nbsp;at processParams (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\router\index.js:582:12)<br> &nbsp; &nbsp;at next (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\router\index.js:291:5)<br> &nbsp; &nbsp;at SendStream.error (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\serve-static\index.js:120:7)<br> &nbsp; &nbsp;at SendStream.emit (node:events:508:28)<br> &nbsp; &nbsp;at SendStream.error (c:\Users\Ethan\Desktop\Ultraviolet-App\node_modules\send\index.js:168:17)</pre>
+</body>
+</html>
